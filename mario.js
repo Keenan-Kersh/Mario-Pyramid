5d3f6a7142b366f184a428d77a2836d4c6a8453f
Keenan Kersh <98796632+Keenan-Kersh@users.noreply.github.com>
2023-01-26 02:51:29 +0000

    Initial commit

---
+++ mario.js
@@ -0,0 +1,35 @@
+
+
+let pyramidHeight = parseInt(prompt("How tall shold the pyramid be?"));
+if(isNaN(pyramidHeight)) {
+	console.log("Error: User input isn't an integer");
+}
+else {
+	printPyramid(pyramidHeight);
+}
+// TODO #2
+// Take in user input for the height
+
+
+/*
+ * printPyramid
+ *
+ * Prints to the console a pyramid of '#' characters of the specified height
+ * For example, if height is 5, the console will look like this:
+ *          ##
+ *         ###
+ *        ####
+ *       #####
+ *      ######
+ */
+function printPyramid(height, width = height+1) {
+		if(height <= 0) {
+			console.log("Error: Pyramid height out of range");
+			return;
+		}
+		if(height != 1) {
+    	printPyramid(height-1, width);
+		}
+		console.log(`${" ".repeat(width - (height + 1))}${"#".repeat(height + 1)}`);
+
+}
